bbfe2bfb3c47fb1594e6d09efcce7de487c8e59a
Marcin Butlak <contact@maker-labs.com>
2011-08-11 22:16:42 +0200

    added default styling

--- Resources/views/Pager/paginate.html.php
+++ Resources/views/Pager/paginate.html.php
@@ -1,7 +1,7 @@
 <ul class="pager">
    <?php if ($pager->isFirstPage() == false): ?>    
-      <li><a href="<?php echo $view['pager']->path($route, $pager->getFirstPage(), $parameters) ?>">&laquo;</a></li>
-      <li><a href="<?php echo $view['pager']->path($route, $pager->getPreviousPage(), $parameters) ?>">&lsaquo;</a></li>
+      <li class="first"><a href="<?php echo $view['pager']->path($route, $pager->getFirstPage(), $parameters) ?>">&laquo;</a></li>
+      <li class="previous"><a href="<?php echo $view['pager']->path($route, $pager->getPreviousPage(), $parameters) ?>">&lsaquo;</a></li>
    <?php endif ?>
    <?php foreach ($pager->getPages() as $page): ?>
       <?php if ($page == $pager->getPage()): ?>
@@ -15,7 +15,7 @@
       <?php endif ?>
    <?php endforeach; ?>  
    <?php if ($pager->isLastPage() == false): ?>
-      <li><a href="<?php echo $view['pager']->path($route, $pager->getNextPage(), $parameters) ?>">&rsaquo;</a></li>
-      <li><a href="<?php echo $view['pager']->path($route, $pager->getLastPage(), $parameters) ?>">&raquo;</a></li>
+      <li class="next"><a href="<?php echo $view['pager']->path($route, $pager->getNextPage(), $parameters) ?>">&rsaquo;</a></li>
+      <li class="last"><a href="<?php echo $view['pager']->path($route, $pager->getLastPage(), $parameters) ?>">&raquo;</a></li>
    <?php endif ?>
 </ul>
